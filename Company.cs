78222581c30544d537c5ff1fe19e78dc1114645b
Viacheslau <mr.tester@tut.by>
2020-05-01 10:32:45 +0300

    Refactoring
    The employees field was rewritten to property

--- Company.cs
+++ Company.cs
@@ -4,19 +4,17 @@ namespace Lesson7
 {
     internal class Company
     {
-        public Employee[] employees = new Employee[10];
-        public string name { get; set; }
+        public Employee[] Employees { get; private set; } = new Employee[10];
+        public string Name { get; set; }
         
         public void Show()
         {
-            Console.WriteLine("Company: {0}", name);
+            Console.WriteLine("Company: {0}", Name);
 
-            foreach (var employee in employees)
+            foreach (var employee in Employees)
             {
-                Console.Write("First Name:{0} Last Name:{1} Passport:{2}", 
+                Console.Write("First Name:{0} Last Name:{1} Passport:{2}" + employee.GetInfo(), 
                                     employee.FirstName, employee.LastName, employee.PassportNumber);
-                employee.GetInfo();
-
             }
         }
     }
